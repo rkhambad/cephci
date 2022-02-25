da15112e18a49bbe179d8fe2171b0bd48b4b140c
anrao19 <83442624+anrao19@users.noreply.github.com>
2022-02-25 17:16:04 +0530

    Update ibm_dns_cleanup.py

--- utility/ibm_dns_cleanup.py
+++ utility/ibm_dns_cleanup.py
@@ -68,8 +68,13 @@ def run(args: Dict):
 
         if "next" in resp.get_result().keys():
             start = resp.get_result()["next"]["href"].split("start=")[-1]
-            for i in range(1, (math.ceil(resp.get_result()["total_count"] / resp.get_result()["limit"]))):
-                list_inst = ibmc_client.list_instances(start=start, limit=7, vpc_name=ibm_cred["vpc_name"])
+            iteration = math.ceil(
+                resp.get_result()["total_count"] / resp.get_result()["limit"]
+            )
+            for i in range(1, iteration):
+                list_inst = ibmc_client.list_instances(
+                    start=start, limit=7, vpc_name=ibm_cred["vpc_name"]
+                )
                 if list_inst.get_status_code() != 200:
                     print("Failed to retrieve instances")
                     return 1
@@ -79,27 +84,34 @@ def run(args: Dict):
                     start = list_inst.get_result()["next"]["href"].split("start=")[-1]
 
         if len(instances) != resp.get_result()["total_count"]:
-            print(f"Failed to list all the instances, total:{resp.get_result()['total_count']} listed:{len(instances)}")
+            print(
+                f"Failed to list all the instances, total:{resp.get_result()['total_count']} listed:{len(instances)}"
+            )
             return 1
 
-        ip_address = [i["primary_network_interface"]["primary_ipv4_address"] for i in instances]
+        print(f"\nlist all the instances, total:{resp.get_result()['total_count']} listed:{len(instances)}")
+        ip_address = [
+            i["primary_network_interface"]["primary_ipv4_address"] for i in instances
+        ]
+        print(ip_address)
 
         for record in records["resource_records"]:
             if record["type"] == "A" and record["rdata"]["ip"] not in ip_address:
-                if record.get("linked_ptr_record"):
-                    print(f"Deleting PTR record {record['linked_ptr_record']['name']}")
+                if not record["name"].startswith("ceph-qe"):
+                    if record.get("linked_ptr_record"):
+                        print(f"Deleting PTR record {record['linked_ptr_record']['name']}")
+#                         dns_client.delete_resource_record(
+#                             instance_id=instance_id,
+#                             dnszone_id=dns_zone_id,
+#                             record_id=record["linked_ptr_record"]["id"],
+#                         )
+
+                    print(f"Deleting Address record {record['name']}")
 #                     dns_client.delete_resource_record(
 #                         instance_id=instance_id,
 #                         dnszone_id=dns_zone_id,
-#                         record_id=record["linked_ptr_record"]["id"],
+#                         record_id=record["id"],
 #                     )
-
-                print(f"Deleting Address record {record['name']}")
-#                 dns_client.delete_resource_record(
-#                     instance_id=instance_id,
-#                     dnszone_id=dns_zone_id,
-#                     record_id=record["id"],
-#                 )
 
         print("\nSuccessfully removed the orphan DNS record from IBM environment\n")
     return 0
